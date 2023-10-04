c62318acd566a1a7b8db1e057f93b19dea6ade18
philipjpark <philipparknyc@gmail.com>
2023-10-04 00:46:23 -0400

    Lab3 Step3: Retrieve gift details

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -1,34 +1,48 @@
 import express from 'express'
-import path from 'path'
-
-//Import the fileURLToPath method from the url module.
-import { fileURLToPath } from 'url'
-
-//Import giftData from the data/gift.js.
-import giftData from '../data/gifts.js'
-
 import GiftsController from '../controllers/gifts.js'
 
-//Convert the import.meta.url property to a file path and store the result in a variable called __filename
-const __filename = fileURLToPath(import.meta.url)
-
-//Create a variable called __dirname to contain the directory name of the current module file.
-const __dirname = path.dirname(__filename)
-
-//Create an Express router.
 const router = express.Router()
-
-//Create a GET route at the / endpoint that responds with status 200 and sends a JSON of the giftData.
-// router.get('/', (req, res) => {
-//     res.status(200).json(giftData)
-//   })
 
 router.get('/', GiftsController.getGifts)
 
-//Create another GET route at the /:giftId endpoint that responds with status 200 and sends the gift.html file (we'll be creating this later).
-router.get('/:giftId', (req, res) => {
-res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
-})
+router.get('/:giftId', GiftsController.getGiftById)
 
-//Export the router.
 export default router
+
+
+// import express from 'express'
+// import GiftsController from '../controllers/gifts.js'
+
+// const router = express.Router()
+
+// router.get('/', GiftsController.getGifts)
+
+// router.get('/:giftId', GiftsController.getGiftById)
+
+// export default router
+
+// import express from 'express'
+// // import path from 'path'
+
+// // import { fileURLToPath } from 'url'
+
+// // import giftData from '../data/gifts.js'
+
+// import GiftsController from '../controllers/gifts.js'
+
+// // const __filename = fileURLToPath(import.meta.url)
+// // const __dirname = path.dirname(__filename)
+
+// const router = express.Router()
+
+
+// router.get('/', GiftsController.getGifts)
+
+
+// // router.get('/:giftId', (req, res) => {
+// // res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
+// // })
+
+// router.get('/:giftId', GiftsController.getGiftById)
+
+// export default router
